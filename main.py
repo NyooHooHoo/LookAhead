18db6fc47501f915daa43c51fb9b0641053eecda
Red4Wiz <saileshshashu@gmail.com>
2023-09-17 00:25:13 -0400

    fixed main

--- main.py
+++ main.py
@@ -3,29 +3,34 @@ from liveimagery import LiveImagery
 from threading import Thread
 from speech import listen_and_record
 from narrator import text_to_speech 
+from vision import *
+from adhawk import *
 
 
-def main():
-	imagery = LiveImagery()
-	eyetracker = EyeTracker(imagery)
+imagery = LiveImagery()
+eyetracker = EyeTracker(imagery)
 
-	eyetracker_thread = Thread(target=eyetracker.start)
-	imagery_thread = Thread(target=imagery.display)
-	eyetracker_thread.start()
-	imagery_thread.start()
-	
-	try:
-		while True:
-			words = listen_and_record().split(' ')
+eyetracker_thread = Thread(target=eyetracker.start)
+imagery_thread = Thread(target=imagery.display)
+eyetracker_thread.start()
+imagery_thread.start()
 
-			if "find" == words[0]:
-				text_to_speech("Finding " + words[1:])
-			else:
+try:
+	while True:
+		input_text = listen_and_record().lower()
+		words = input_text.lower().split(' ')
+
+		if words[0] == "find":
+			text_to_speech("Finding " + ' '.join(words[1:]))
+
+		elif input_text == "what am i looking at"
+			# looking_at(localize_objects(),)
+
+		elif input_text == "turn obstacle detection mode on"
+			text_to_speech("Turning obstacle detection mode on")
+
+		else:
 				break
-	except (KeyboardInterrupt, SystemExit):
-		eyetracker.shutdown()
-		imagery.shutdown()
-
-
-if __name__ == "__main__":
-	main()
+except (KeyboardInterrupt, SystemExit):
+	eyetracker.shutdown()
+	imagery.shutdown()
